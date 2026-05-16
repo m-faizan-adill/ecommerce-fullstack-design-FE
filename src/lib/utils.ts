d94cddead215c35a1b0b7aff393588e857943b93
m-faizan-adill <adilfaizan423@gmail.com>
2026-05-17 03:44:18 +0500

    feat(utils): add formatPrice helper for currency formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatPrice = (n: number) => new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(n);
