30e55ac4cad668875f925ac2cc6fef6ac1e9b3db
m-faizan-adill <adilfaizan423@gmail.com>
2026-05-23 19:35:04 +0500

    feat(ui): add reusable Container layout component

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import type { HTMLAttributes, ReactNode } from "react";
+
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+export function Container({ className, children, ...props }: ContainerProps) {
+  return (
+    <div
+      className={cn("mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
