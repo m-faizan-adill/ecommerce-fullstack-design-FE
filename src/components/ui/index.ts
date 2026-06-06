bfcd8036f405b97dc3ecb93d5b16a12a3d21bd76
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-06 19:48:14 +0500

    refactor(ui): export common components from barrel file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,6 @@ export { ChevronIcon } from "./ChevronIcon";
 export { HamburgerIcon } from "./HamburgerIcon";
 export { FlagIcon } from "./FlagIcon";
 export { default as Breadcrumbs } from "./Breadcrumbs";
+export { default as Button } from "./Button";
+export { Checkbox, Radio } from "./FormChecks";
+export { default as Rating } from "./Rating";
