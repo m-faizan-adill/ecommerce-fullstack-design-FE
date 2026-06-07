e77f76ac64e97ab4c830a095ea26e136cf234d87
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-07 09:51:05 +0500

    refactor(ui): remove icon exports from ui barrel file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,4 @@
 export { Dropdown, DropdownTrigger, DropdownContent, DropdownItem } from "./Dropdown";
-export { ChevronIcon } from "./ChevronIcon";
-export { HamburgerIcon } from "./HamburgerIcon";
-export { FlagIcon } from "./FlagIcon";
 export { default as Breadcrumbs } from "./Breadcrumbs";
 export { default as Button } from "./Button";
 export { Checkbox, Radio } from "./FormChecks";
