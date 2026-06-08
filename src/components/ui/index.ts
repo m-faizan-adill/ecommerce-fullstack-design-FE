ca195e9917fd4776ecb1880c2e7df2406afa7023
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-09 02:14:15 +0500

    All exported FormSelect from one index file.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Breadcrumbs } from "./Breadcrumbs";
 export { default as Button } from "./Button";
 export { Checkbox, Radio } from "./FormChecks";
 export { default as Rating } from "./Rating";
+export * from "./FormSelect";
