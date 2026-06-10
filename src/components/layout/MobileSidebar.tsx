98f950945ebc5b9435abd28a6bd4e5d522b1630f
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-10 19:29:05 +0500

    fix(sidebar): enable scrolling in mobile sidebar

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -35,7 +35,7 @@ export default function MobileSidebar({ isOpen, onClose }: MobileSidebarProps) {
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 left-0 h-full w-72 bg-[#FFFFFF] z-50 transform transition-transform duration-300 ease-in-out lg:hidden ${
+        className={`fixed top-0 left-0 h-full w-72 overflow-y-auto scrollbar-hide bg-[#FFFFFF] z-50 transform transition-transform duration-300 ease-in-out lg:hidden ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
